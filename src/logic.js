7e258b56982ec6ee1c5cba2149dd74c3b55d3f7a
utensil <utensilcandel@gmail.com>
2016-06-25 13:07:17 +0800

    Make Logic configurable and improve testcases

--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const _ = require('lodash');
+
 const REPEAT_COUNT = 1000;
 const SUCCESS_RATE = 1.0;
 const PI = 3.141592653589793238462643383279;
 const MAX_TIMEOUT_IN_SECS = 10;
-const THRESHOLD = 0.1;
+const THRESHOLD = 0.9;
 
 const DUMMY_MAP = {
   'etc.': 'et cetera',
@@ -16,65 +18,76 @@ const DUMMY_MAP = {
   'dummy': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 };
 
-const returnResult = (result, resolve, reject) => {
-  let rnd = Math.random();
+const DUMMY_VALUES = _.values(DUMMY_MAP);
 
-  if(rnd <= SUCCESS_RATE) {
-    resolve({
-      status: true,
-      result: result
-    });
-  } else {
-    resolve({
-      status: false
-    });
-  }
-};
-
-module.exports = class Logic {
-  constructor() {
-
+class Logic {
+  constructor(config) {
+    this.config = _.assign({}, {
+      REPEAT_COUNT,
+      SUCCESS_RATE,
+      MAX_TIMEOUT_IN_SECS,
+      THRESHOLD
+    }, config);
   }
 
   doMath(req) {
-    return new Promise(function(resolve, reject) {
-      let ret = 0;
+    const REPEAT_COUNT = this.config.REPEAT_COUNT;
+
+    return new Promise((resolve, reject) => {
+      let res = 0;
 
       for (var i = 0; i < REPEAT_COUNT; i++) {
-        ret += Math.sin(Math.random() * i * PI);
+        res += Math.sin(Math.random() * i * PI);
       }
 
-      returnResult(ret, resolve, reject);
+      this.__returnResult(res, resolve, reject);
     });
   }
 
   sleep(req) {
+    const MAX_TIMEOUT_IN_SECS = this.config.MAX_TIMEOUT_IN_SECS;
+
     let seconds = (req || {}).seconds || Math.random() * MAX_TIMEOUT_IN_SECS;
 
-    return new Promise(function(resolve, reject) {
-      setTimeout(function () {
-        returnResult(seconds, resolve, reject);
+    return new Promise((resolve, reject) => {
+      setTimeout( () => {
+        this.__returnResult(seconds, resolve, reject);
       }, seconds * 1000);
     });
   }
 
   query(req) {
-    return new Promise(function(resolve, reject) {
-      let hasReturned = false;
+    const THRESHOLD = this.config.THRESHOLD;
 
-      for(var key in DUMMY_MAP) {
-        if (DUMMY_MAP.hasOwnProperty(key)) {
-          if (Math.random() < THRESHOLD) {
-            returnResult(DUMMY_MAP[key], resolve, reject);
-            hasReturned = true;
-            break;
-          }
-        }
+    return new Promise((resolve, reject) => {
+
+      if (Math.random() < THRESHOLD) {
+        let res = _.sample(DUMMY_VALUES);
+        this.__returnResult(res, resolve, reject);
+      } else {
+        this.__returnResult(DUMMY_MAP.dummy, resolve, reject);
       }
 
-      if(!hasReturned) {
-        returnResult(DUMMY_MAP.dummy, resolve, reject);
-      }
     });
   }
+
+  __returnResult(value, resolve, reject) {
+    let rnd = Math.random();
+
+    if(rnd <= this.config.SUCCESS_RATE) {
+      resolve({
+        status: true,
+        value: value
+      });
+    } else {
+      resolve({
+        status: false
+      });
+    }
+  }
 }
+
+module.exports = {
+  Logic,
+  DUMMY_VALUES
+};
--- test/index.js
+++ test/index.js
@@ -1,32 +1,63 @@
 'use strict';
 
 const expect = require('chai').expect;
-const Logic = require('../src/logic');
+const logic = require('../src/logic');
+const debug = require('debug')('test');
 
 describe('Logic', () => {
 
-  let logic;
+  const CONFIG = {
+    SUCCESS_RATE: 1.0,
+    MAX_TIMEOUT_IN_SECS: 1.0
+  };
+
+  let l;
 
   beforeEach((done) => {
-    logic = new Logic();
+    l = new logic.Logic(CONFIG);
     done();
   })
 
   it('can doMath()', function(done) {
-    logic.doMath().then((ret) => {
+    l.doMath().then((ret) => {
+      expect(ret).to.be.ok;
+      expect(ret.status).to.be.true;
+      expect(ret.value).to.be.a('number');
+
+      debug('doMath() =>', ret.value);
+
       done();
+    }).catch((err) => {
+      done(err);
     });
   });
 
   it('can sleep()', function(done) {
-    logic.sleep().then((ret) => {
+    l.sleep().then((ret) => {
+      expect(ret).to.be.ok;
+      expect(ret.status).to.be.true;
+      expect(ret.value).to.be.a('number');
+      expect(ret.value).to.be.within(0, CONFIG.MAX_TIMEOUT_IN_SECS);
+
+      debug('sleep() =>', ret.value);
+
       done();
+    }).catch((err) => {
+      done(err);
     });
   });
 
   it('can query()', function(done) {
-    logic.query().then((ret) => {
+    l.query().then((ret) => {
+      expect(ret).to.be.ok;
+      expect(ret.status).to.be.true;
+      expect(ret.value).to.be.a('string');
+      expect(logic.DUMMY_VALUES).to.include(ret.value);
+      debug('query() =>', ret.value);
+
       done();
+    }).catch((err) => {
+      done(err);
     });
   });
 });
